d3d5dac4e5127e2048cfb31d7e9d713c8a87b370
M-Curtis <curtis.mullett@gmail.com>
2016-11-14 16:55:22 +0000

    Add project files.

---
+++ src/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var db = new Context();
+            foreach (var dbCompany in db.Companies)
+            {
+                Console.WriteLine(dbCompany.Id);
+            }
+        }
+    }
+
+    public class Context
+    {
+        public DbSet<Company> Companies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=WIN-B1N7P59KFSN\\Administrator;Integrated Security=True");
+        }
+    }
+
+    public class Company
+    {
+        public int Id;
+        public string CompanyName;
+        public string Website;
+        public string Notes;
+
+        public Company() { }
+        
+    }
+}
